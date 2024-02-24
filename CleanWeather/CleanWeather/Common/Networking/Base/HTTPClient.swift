80c74648166e27d643f20953af5438e44ea290fc
Omer Rahmanovic <omer.6g@gmail.com>
2024-02-24 16:11:20 +0100

    expand networking layer

--- CleanWeather/CleanWeather/Common/Networking/Base/HTTPClient.swift
+++ CleanWeather/CleanWeather/Common/Networking/Base/HTTPClient.swift
@@ -12,14 +12,12 @@ protocol HTTPClient {
 }
 
 extension HTTPClient {
-    func sendRequest<T: Decodable>(
-        endpoint: Endpoint,
-        responseModel: T.Type
-    ) async -> Result<T, RequestError> {
+    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, RequestError> {
         var components = URLComponents()
         components.scheme = endpoint.scheme
         components.host = endpoint.host
         components.path = endpoint.path
+        
         if let params = endpoint.parameters{
             components.setQueryItems(with: params)
         }
@@ -28,12 +26,10 @@ extension HTTPClient {
             return .failure(.invalidURL)
         }
         
-        debugPrint("URL request: \(url)")
-        
         var request = URLRequest(url: url)
         request.httpMethod = endpoint.method.rawValue
         request.allHTTPHeaderFields = endpoint.header
-
+        
         if let body = endpoint.body {
             debugPrint("Body:", body)
             do {
@@ -42,7 +38,6 @@ extension HTTPClient {
                 debugPrint("Error while parsing body into JSON!", error)
             }
         }
-        
         
         do {
             let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
@@ -65,8 +60,26 @@ extension HTTPClient {
                 }
                 debugPrint("Response data:", decodedResponse)
                 return .success(decodedResponse)
+            case 300:
+                return .failure(.multipleChoices)
+            case 301:
+                return .failure(.movedPermanently)
+            case 304:
+                return .failure(.notModified)
+            case 400:
+                return .failure(.badRequest)
             case 401:
                 return .failure(.unauthorized)
+            case 403:
+                return .failure(.forbidden)
+            case 404:
+                return .failure(.notFound)
+            case 409:
+                return .failure(.conflict)
+            case 500:
+                return .failure(.internalServerError)
+            case 505:
+                return .failure(.httpVersionNotSupported)
             default:
                 return .failure(.unexpectedStatusCode)
             }
