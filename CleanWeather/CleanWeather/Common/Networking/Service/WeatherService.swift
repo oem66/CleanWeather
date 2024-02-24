4ed6e4da7bd20209f4fe30e971afb3b0d823fece
Omer Rahmanovic <omer.6g@gmail.com>
2024-02-24 16:07:48 +0100

    add Presentation layer

---
+++ CleanWeather/CleanWeather/Common/Networking/Service/WeatherService.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherService.swift
+//  CleanWeather
+//
+//  Created by Omer Rahmanovic on 24. 2. 2024..
+//
+
+import Foundation
+
+protocol WeatherServiceProtocol {
+    
+}
+
+final class WeatherService: WeatherServiceProtocol {
+    
+}
---
+++ CleanWeather/CleanWeather/Features/Weather/Presentation/View Model/WeatherViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherViewModel.swift
+//  CleanWeather
+//
+//  Created by Omer Rahmanovic on 24. 2. 2024..
+//
+
+import Foundation
+
+final class WeatherViewModel: ObservableObject {
+    
+}
