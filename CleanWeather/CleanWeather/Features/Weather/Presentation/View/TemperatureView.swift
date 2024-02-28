9a655eb8bea6e63eeadf5ec257a27a176c176e7b
Omer Rahmanovic <omer.6g@gmail.com>
2024-02-28 20:20:04 +0100

    change UI

--- CleanWeather/CleanWeather/Features/Weather/Presentation/View/TemperatureView.swift
+++ CleanWeather/CleanWeather/Features/Weather/Presentation/View/TemperatureView.swift
@@ -19,7 +19,7 @@ struct TemperatureView: View {
                 .foregroundColor(.black)
                 .multilineTextAlignment(.center)
                 .padding(.top, 40)
-            Image("cloudy")
+            Image("sun")
                 .resizable()
                 .frame(width: 220, height: 190)
             Text("\(viewModel.weatherData.currentWeather?.temperature.nextUp ?? 0.0, specifier: "%.1f")°C")
