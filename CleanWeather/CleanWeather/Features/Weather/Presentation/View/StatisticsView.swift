0d547958de4dd8e9721db2e52971f26f2b12bb2c
Omer Rahmanovic <omer.6g@gmail.com>
2024-03-10 20:08:55 +0100

    add measurement units

--- CleanWeather/CleanWeather/Features/Weather/Presentation/View/StatisticsView.swift
+++ CleanWeather/CleanWeather/Features/Weather/Presentation/View/StatisticsView.swift
@@ -27,20 +27,20 @@ struct StatisticsView: View {
                 .padding(.leading, 15)
             Group {
                 LazyVGrid(columns: columns, spacing: 20) {
-                    StatisticCard("AQI", nil, 30)
-                    StatisticCard("UV Index", nil, viewModel.weatherData.currentWeather?.uvIndex)
+                    StatisticCard("AQI", nil, 30, unit: "")
+                    StatisticCard("UV Index", nil, viewModel.weatherData.currentWeather?.uvIndex, unit: "")
                 }
                 LazyVGrid(columns: columns, spacing: 20) {
-                    StatisticCard("Pressure", viewModel.weatherData.currentWeather?.pressure, nil)
-                    StatisticCard("Humidity", viewModel.weatherData.currentWeather?.humidity, nil)
+                    StatisticCard("Pressure", viewModel.weatherData.currentWeather?.pressure, nil, unit: "mb")
+                    StatisticCard("Humidity", viewModel.weatherData.currentWeather?.humidity, nil, unit: "")
                 }
                 LazyVGrid(columns: columns, spacing: 20) {
-                    StatisticCard("Feels Like", viewModel.weatherData.currentWeather?.temperatureApparent, nil)
-                    StatisticCard("Visibility", viewModel.weatherData.currentWeather?.visibility, nil)
+                    StatisticCard("Feels Like", viewModel.weatherData.currentWeather?.temperatureApparent, nil, unit: "°C")
+                    StatisticCard("Visibility", viewModel.weatherData.currentWeather?.visibility, nil, unit: "m")
                 }
                 LazyVGrid(columns: columns, spacing: 20) {
-                    StatisticCard("Wind Speed", viewModel.weatherData.currentWeather?.windSpeed, nil)
-                    StatisticCard("Wind Gust", viewModel.weatherData.currentWeather?.windGust, nil)
+                    StatisticCard("Wind Speed", viewModel.weatherData.currentWeather?.windSpeed, nil, unit: "km/h")
+                    StatisticCard("Wind Gust", viewModel.weatherData.currentWeather?.windGust, nil, unit: "km/h")
                 }
             }
             .padding(.horizontal, 20)
@@ -49,7 +49,7 @@ struct StatisticsView: View {
     }
     
     @ViewBuilder
-    private func StatisticCard(_ title: String, _ doubleValue: Double?, _ intValue: Int?) -> some View {
+    private func StatisticCard(_ title: String, _ doubleValue: Double?, _ intValue: Int?, unit: String) -> some View {
         VStack(alignment: .leading) {
             HStack {
                 Text(title)
@@ -58,17 +58,28 @@ struct StatisticsView: View {
                     .foregroundColor(.gray)
                 Spacer()
             }
-            if let doubleValue {
-                Text("\(doubleValue, specifier: "%.1f")")
-                    .font(.custom("Avenir-Medium", size: 20))
+            HStack {
+                if let doubleValue {
+                    Text("\(doubleValue, specifier: "%.1f")")
+                        .font(.custom("Avenir-Medium", size: 20))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                } else if let intValue {
+                    Text("\(intValue)")
+                        .font(.custom("Avenir-Medium", size: 20))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                }
+                
+                Text(unit)
+                    .font(.custom("Avenir-Medium", size: 18))
                     .fontWeight(.bold)
-                    .foregroundColor(.black)
-            } else if let intValue {
-                Text("\(intValue)")
-                    .font(.custom("Avenir-Medium", size: 20))
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
+                    .foregroundColor(.gray)
+                    .padding(.trailing, 3)
+                
+                Spacer()
             }
+            
         }
         .padding(.vertical, 25)
         .padding(.horizontal, 10)
---
+++ README.md
@@ -0,0 +1,2 @@
+# CleanWeather
+Weather app made using Apple WeatherKit and Clean Architecture with MVVM in presentation layer.
