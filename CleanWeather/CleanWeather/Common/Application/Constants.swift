fc9fd10b7f4204b92020a2421de909c7009513b2
Omer Rahmanovic <omer.6g@gmail.com>
2024-04-06 21:08:15 +0200

    add constant colors

---
+++ CleanWeather/CleanWeather/Common/Application/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  CleanWeather
+//
+//  Created by Omer Rahmanovic on 6. 4. 2024..
+//
+
+import Foundation
+import SwiftUI
+
+final class Constants {
+    // MARK: - Application colors
+    static let defaultBackground = Color(red: 0/255, green: 0/255, blue: 0/255)
+    
+    // MARK: - Air Quality colors
+    static let aqiGood = Color(red: 0/255, green: 0/255, blue: 0/255)
+    static let aqiModerate = Color(red: 0/255, green: 0/255, blue: 0/255)
+    static let aqiUnhealthySensitiveGroups = Color(red: 0/255, green: 0/255, blue: 0/255)
+    static let aqiUnhealthy = Color(red: 0/255, green: 0/255, blue: 0/255)
+    static let aqiVeryUnhealthy = Color(red: 0/255, green: 0/255, blue: 0/255)
+    static let aqiHazardous = Color(red: 0/255, green: 0/255, blue: 0/255)
+    
+}
