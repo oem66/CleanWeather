267f9fe012b2690fdfae448cb6c389ba9b3d372c
Omer Rahmanovic <omer.6g@gmail.com>
2024-05-14 14:44:56 +0200

    UI changes on select location view

--- CleanWeather/CleanWeather/Features/Select Location/Presentation/View/SelectLocationView.swift
+++ CleanWeather/CleanWeather/Features/Select Location/Presentation/View/SelectLocationView.swift
@@ -9,9 +9,36 @@ import Foundation
 import SwiftUI
 
 struct SelectLocationView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var viewModel: WeatherViewModel
+    
+    var backButton: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            ZStack {
+                Color(.white)
+                Image(systemName: "chevron.left")
+                    .foregroundColor(Constants.defaultBackground)
+                    .padding(10)
+            }
+            .clipShape(Circle())
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Select Location")
+            Spacer()
+            HStack {
+                Spacer()
+                Text("Select Location")
+                Spacer()
+            }
+            Spacer()
         }
+        .background(Constants.defaultBackground)
+        .navigationTitle("Select Location")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
     }
 }
