1b8651f904fc3178b8d8a976d79a762928df1379
Omer Rahmanovic <omer.6g@gmail.com>
2024-05-16 13:09:12 +0200

    bump minimum version

--- CleanWeather/CleanWeather/Features/Select Location/Presentation/View/SelectLocationView.swift
+++ CleanWeather/CleanWeather/Features/Select Location/Presentation/View/SelectLocationView.swift
@@ -14,68 +14,64 @@ struct SelectLocationView: View {
     @Binding var showSheetView: Bool
     
     var body: some View {
-        if #available(iOS 16.4, *) {
+        VStack {
             VStack {
-                VStack {
-                    HStack {
-                        Spacer()
-                        Text("Select city")
-                            .font(.custom("Avenir-Medium", size: 25))
-                            .fontWeight(.heavy)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                    .padding(.top, 20)
-                    
-                    VStack(alignment: .center) {
-                        TextField("Enter city name",
-                                  text: $viewModel.cityName,
-                                  onCommit: viewModel.fetchCoordinates)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        if let coordinates = viewModel.coordinates {
-                            Text("Location: \(viewModel.locationName), coordinates: \(coordinates.latitude) - \(coordinates.longitude)")
-                                .font(.custom("Avenir-Medium", size: 16))
-                                .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .multilineTextAlignment(.leading)
-                                .lineLimit(4)
-                                .padding()
-                        }
-                    }
-                    .padding(.vertical, 20)
-                    
-                    HStack {
-                        Spacer()
-                        Button {
-                            showSheetView.toggle()
-                        } label: {
-                            HStack {
-                                Spacer()
-                                Text("Select")
-                                    .font(.custom("Avenir-Medium", size: 20))
-                                    .fontWeight(.heavy)
-                                    .foregroundColor(.white)
-                                    .padding(.vertical, 10)
-                                Spacer()
-                            }
-                            .background(.green)
-                            .cornerRadius(15)
-                        }
-                        Spacer()
-                    }
-                    .padding(.horizontal, 15)
-                    .padding(.bottom, 30)
+                HStack {
+                    Spacer()
+                    Text("Select city")
+                        .font(.custom("Avenir-Medium", size: 25))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.black)
+                    Spacer()
                 }
-                .background(.white)
-                .cornerRadius(15)
+                .padding(.top, 20)
+                
+                VStack(alignment: .center) {
+                    TextField("Enter city name",
+                              text: $viewModel.cityName,
+                              onCommit: viewModel.fetchCoordinates)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    if let coordinates = viewModel.coordinates {
+                        Text("Location: \(viewModel.locationName), coordinates: \(coordinates.latitude) - \(coordinates.longitude)")
+                            .font(.custom("Avenir-Medium", size: 16))
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.leading)
+                            .lineLimit(4)
+                            .padding()
+                    }
+                }
+                .padding(.vertical, 20)
+                
+                HStack {
+                    Spacer()
+                    Button {
+                        showSheetView.toggle()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Select")
+                                .font(.custom("Avenir-Medium", size: 20))
+                                .fontWeight(.heavy)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }
+                        .background(.green)
+                        .cornerRadius(15)
+                    }
+                    Spacer()
+                }
                 .padding(.horizontal, 15)
+                .padding(.bottom, 30)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .presentationBackground(.clear.opacity(0.5))
-            .edgesIgnoringSafeArea(.all)
-        } else {
-            // Fallback on earlier versions
+            .background(.white)
+            .cornerRadius(15)
+            .padding(.horizontal, 15)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .presentationBackground(.clear.opacity(0.5))
+        .edgesIgnoringSafeArea(.all)
     }
 }
