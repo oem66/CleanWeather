1a032b128f963bb08fb5501baab59f166a2565e4
Omer Rahmanovic <omer.6g@gmail.com>
2024-06-14 11:09:23 +0200

    add local repository

---
+++ CleanWeather/CleanWeather/Features/Weather/Data/Local/WeatherLocalRepository.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocalRepository.swift
+//  CleanWeather
+//
+//  Created by Omer Rahmanovic on 14. 6. 2024..
+//
+
+import Foundation
+import CoreData
+
+final class WeatherLocalRepository {
+    
+    func getWeather() {
+        
+    }
+}
